cb3113dd759572223fc0744a6d938c326006ee7a
TheEmilyC <141741116+TheEmilyC@users.noreply.github.com>
2026-04-06 12:43:24 -0400

    feat: add PROMPT_FILE env option

--- src/lib/env-variables.ts
+++ src/lib/env-variables.ts
@@ -2,6 +2,8 @@ export const ENVIRONMENT = process.env.NODE_ENV;
 export const DATABASE_URL = process.env.DATABASE_URL ?? "";
 export const WORKING_DIRECTORY = process.cwd();
 export const OBSIDIAN_URL = process.env.OBSIDIAN_URL ?? "http://127.0.0.1";
+export const PROMPT_FILE =
+  process.env.PROMPT_FILE ?? "main-prompts/writer.json";
 
 export const LOREBOOK_TAG = process.env.LOREBOOK_TAG ?? "#lorebook";
 export const LOREBOOK_ALWAYS_TAG =
