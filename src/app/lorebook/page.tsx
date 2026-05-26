790f253b8525ed904d7ff3c812d535ea1892f406
TheEmilyC <141741116+TheEmilyC@users.noreply.github.com>
2026-05-25 23:01:03 -0400

    fix: docker build (#126)

--- src/app/lorebook/page.tsx
+++ src/app/lorebook/page.tsx
@@ -1,6 +1,8 @@
 import { LorebookList } from "@/app/lorebook/_components/lorebook-list";
 import { getLorebookEntityDtoList } from "@/app/lorebook/_lib/data";
 
+export const dynamic = "force-dynamic";
+
 export default async function LorebookPage() {
   const lorebooks = await getLorebookEntityDtoList();
   return <LorebookList lorebooks={lorebooks} />;
